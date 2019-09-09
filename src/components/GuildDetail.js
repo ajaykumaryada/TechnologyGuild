8a180029f7bf039270581acd8a3b3ce66b6a0d54
ajaykumaryada <ajay.yada@gmail.com>
2019-09-09 11:36:32 -0400

    test from work

--- src/components/GuildDetail.js
+++ src/components/GuildDetail.js
@@ -7,6 +7,7 @@ const GuildDetail = props => {
       <Image source={props.imageSource} />
       <Text>{props.title}</Text>
       <Text>Image Score - {props.imageScore}</Text>
+      <Text>Test from office</Text>
     </View>
   );
 };
